4f427ccfc82d6ed5b5d5dbc835954d5630e06cc9
MikiKru <michal_kruczkowski@o2.pl>
2020-05-31 12:51:20 +0200

    project v8

--- coface_scrapping.py
+++ coface_scrapping.py
@@ -18,28 +18,19 @@ class CountryRisk:
         return "%s, %s, %s, %s, %s" % (self.country, self.ref, self.area, self.risk, self.climate)
 class CofaceScrapping:
     def __init__(self):
-        print("Jestem w kontruktorze")
         self.countryRisks = []
         self.area = {0 : "Africa", 1 : "America", 2 : "Asia", 3 : "CIS", 4 : "Europe", 5 : "Middle-East"}
-        pass
     def getTablesByPandas(self):
         # pobieramy tabele z html do listy coface
         coface = pd.read_html(
                 'https://www.coface.com/Economic-Studies-and-Country-Risks/Comparative-table-of-country-assessments')
         for c in coface:
             print(c)
-    # def printResults(self):
-    #     for i in self.countryRisks.index:
-    #         print(self.countryRisks['Country risk assessment'][i])
     def getHtmlCodeByBs4(self):
         page = requests.get('https://www.coface.com/Economic-Studies-and-Country-Risks/Comparative-table-of-country-assessments')
         html = bs4.BeautifulSoup(page.content, 'html.parser')
-
         tables = html.find_all('table', attrs={'class' : 'eval_tab'})   # lista 6 tabel
         headers = []
-        data = []
-        refs = []
-        countries = []
         for i,t in enumerate(tables):
             headers.append(str(tables[i].findAll('th'))\
                 .replace('</th>','')\
@@ -61,10 +52,11 @@ class CofaceScrapping:
             for index, a in enumerate(tableMarkerA):
                 self.countryRisks.append(CountryRisk(
                     str(a).replace('</a>', '').split('">')[1],
-                    str(a).replace('<a href="', '').split('">')[0],
+                    "https://www.coface.com/" + str(a).replace('<a href="', '').split('">')[0],
                     headers[i][0],
                     risks[index],
                     climates[index]))
+    def printResults(self):
         for cs in self.countryRisks:
             print(cs)
 
@@ -72,4 +64,4 @@ class CofaceScrapping:
 cs = CofaceScrapping()      # utworzenie obiektu i wywołanie konstruktora domyślnego
 # cs.getTablesByPandas()      # wywołanie metody
 cs.getHtmlCodeByBs4()
-# cs.printResults()
+cs.printResults()
